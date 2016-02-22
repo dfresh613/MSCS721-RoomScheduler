33a1e442a996a754149341ff1422ac87d0297613
derohde <derohde>
2016-02-22 17:58:37 -0500

    beginnings of junit testSuite

---
+++ test/rsTestCases/RoomSchedulerTestSuite.java
@@ -0,0 +1,12 @@
+package rsTestCases;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CreateRoom.class })
+public class RoomSchedulerTestSuite {
+
+}
